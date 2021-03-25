8f214c2b99db30a557d97f5c9a134ba5bdf827f7
Lutz Kinschert <68464349+LuPat@users.noreply.github.com>
2021-03-25 11:14:16 +0100

    add base function file

---
+++ functions.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def pre_cleaning(data):
+    '''
+    lower incoming columns, drop not needed columns and rename processed columns
+    '''
+    data.columns = map(str.lower, data.columns)
+    data.drop(['vp: design parent'], axis=1, inplace=True)
+    data.rename(columns={'vp: production plant': 'plant',
+                     'vp: manufacturer group' : 'brand',
+                     'vp: country': 'country', 
+                     'vp: region': 'region'}, inplace=True)
+    return data
+
+def cars_wide_to_long(data):
+    '''
+    change dataset wide to long with a melt function
+    '''
+    data = data.melt(id_vars = ['plant', 'country', 'region', 'brand'], 
+              var_name = 'date', 
+              value_name = 'carbuilds')
+    return data
+
+# add if statement to avoid auto call of function after import
+# SEE YOUTUBE timeline‚
