2c0b0e38672d94004b789bfae62fab4d4b1b290c
Lutz Kinschert <68464349+LuPat@users.noreply.github.com>
2021-03-25 15:28:22 +0100

    add theme for layout and graph styles

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 import flask
 import dash
+import plotly.io as pio
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
+pio.templates.default = 'plotly_dark'
 
-app = dash.Dash(__name__)
-server = app.server‚
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+server = app.server
 
 app.layout = html.Div('My Dashboard')
+
 
 
 if __name__ == '__main__':
