218f901586246ac2ef149007821993259d0e1c61
Lutz Kinschert <68464349+LuPat@users.noreply.github.com>
2021-03-28 21:09:15 +0200

    worked on callback 3

--- application.py
+++ application.py
@@ -22,13 +22,14 @@ pio.templates.default = 'plotly'
 # Import Data, set 'date' column to datetime and as index
 cars = pd.read_csv('./data/car_builds_long_format.csv', index_col=0, parse_dates=True)
 cars['date'] = pd.to_datetime(cars['date'])
+df = cars.groupby(['region', 'country', 'customer', 'plant', 'date']).sum().reset_index()
 
 # define variables with unique names for customer, plant, country and region
-customer = [{'label':i, 'value': i} for i in cars['customer'].unique()]
-plant = [{'label':i, 'value': i} for i in cars['plant'].unique()]
-country = [{'label':i, 'value': i} for i in cars['country'].unique()]
-region = [{'label':i, 'value': i} for i in cars['region'].unique()]
-df = px.data.gapminder()
+customer = [{'label':i, 'value': i} for i in df['customer'].unique()]
+plant = [{'label':i, 'value': i} for i in df['plant'].unique()]
+country = [{'label':i, 'value': i} for i in df['country'].unique()]
+region = [{'label':i, 'value': i} for i in df['region'].unique()]
+#df = px.data.gapminder()
 
 # activate the dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
@@ -46,17 +47,12 @@ card_main = dbc.Card(
                     "Please choose region and customer",
                     className="card-text",
                 ),
-                dbc.FormGroup([
-                    
-                    dbc.Checklist(
-                        options=region, value=[], id="checklist_region", inline=True
-                    ),
-                ]),
- 
-                dcc.Dropdown(id='customer_choice', options=customer,
-                             value='BMW', placeholder='select Customer', clearable=True, style={"color": "#000000"}),
-                dcc.Dropdown(id='plant_choice', options=plant,
-                              value=' ', clearable=True, style={"color": "#000000"}),
+                dcc.Dropdown(id='region_choice', options=region,
+                             value=None, placeholder='select Region', clearable=True, style={"color": "#000000"}),
+                dcc.Dropdown(id='customer_choice', options= [],
+                             value='', placeholder='select Customer', clearable=True, style={"color": "#000000"}),
+                dcc.Dropdown(id='plant_choice', options=[],
+                              value='', placeholder='select Customer Plant', clearable=True, style={"color": "#000000"}),
                  #dbc.CardLink("GirlsWhoCode", href="https://girlswhocode.com/", target="_blank"),
             ]
         ),
@@ -104,7 +100,7 @@ app.layout = html.Div(children=[
                 dbc.CardBody([
                     ("DIsplay the KPI NUmber"),
                 ]),
-            ]),
+            ],),
         ),
         dbc.Col(
             dbc.Card([
@@ -124,51 +120,75 @@ app.layout = html.Div(children=[
             ), 
         dbc.Col(dbc.Card(children=[
             card_main,
-    ]), width=4,),
+    ]), ),
     ]),
     ], className='divFrame')
 ], className='divBorder')
 
-
+# region selection
 @app.callback(
-    Output('customer_choice', 'options'),
-    Output('customer_choice', 'value'),
-    [Input('checklist_region', 'value')]
+    Output(component_id ='customer_choice', component_property='options'),
+    #Output(component_id='customer_choice', component_property='value'),
+    Input(component_id='region_choice', component_property='value')
 )
-def update_dropdown(selected_region):
-    df = cars
+
+def update_customer(selected_region):
     filter_region = df[df['region'] == selected_region]
-    return country_options
+    customer_options = [{'label': i, 'value': i} for i in sorted(filter_region['customer'].unique())]
+    #print(customer_options)
+    return customer_options#, customer_options[0]['value']
 
 @app.callback(
-    Output('carbuilds_graph', 'figure'),
-    [Input('customer_choice', 'value')],
-    [Input('customer_choice', 'value')]
-    )
+    Output('plant_choice', 'options'),
+    #Output('plant_choice', 'values'),
+    #Input('customer_choice', 'options'),
+    Input('customer_choice', 'value')
+)
+def update_plant(selected_customer):
+    print(selected_customer)
+    if type(selected_customer) !=str:
+        val = selected_customer
+        print(val)
+        filter_customer = df[df['customer'] == val]
+        
+        #plant_options = [{'label': x, 'value': x} for x in sorted(filter_customer['plant'].unique())]
+    return filter_customer, #plant_options[0]['value'], 
 
-def update_graph(selected_customer, selected_data):
-    df = cars.groupby(['region', 'country', 'customer', 'date']).sum().reset_index()
-    customer_filter = df[df['customer'] == selected_customer]
-    line_fig = line_fig = px.bar(customer_filter, x='date', y='carbuilds', title=f'Carbuilds in {selected_customer}')
-    line_fig.update_xaxes(rangeslider_visible=True)
-    line_fig.update_xaxes(
-        rangeslider_visible=True,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-        ])
-    ))
-    line_fig.update_layout(
-        font_family='Courier New',
-        font_color='Black',
-        title_font_family='Times New Roman',
-        legend_title_font_color="grey"
-    )   
-    return line_fig
+
+# select customer plant
+# @app.callback(
+#     Output('carbuilds_graph', 'figure'),
+#     Input('plant_choice', 'value')
+# )
+# def select_plant(selected_plant):
+#     select_plant = df[df.plant == selected_plant]
+#     return select_plant
+
+
+
+
+# def update_graph(selected_plant):
+#     df_graph = df[df.plant == selected_plant]
+#     line_fig = line_fig = px.bar(df_graph, x='date', y='carbuilds', title=f'Carbuilds in {selected_plant}')
+#     line_fig.update_xaxes(rangeslider_visible=True)
+#     line_fig.update_xaxes(
+#         rangeslider_visible=True,
+#         rangeselector=dict(
+#             buttons=list([
+#                 dict(count=1, label="1m", step="month", stepmode="backward"),
+#                 dict(count=6, label="6m", step="month", stepmode="backward"),
+#                 dict(count=1, label="YTD", step="year", stepmode="todate"),
+#                 dict(count=1, label="1y", step="year", stepmode="backward"),
+#                 dict(step="all")
+#         ])
+#     ))
+#     line_fig.update_layout(
+#         font_family='Courier New',
+#         font_color='Black',
+#         title_font_family='Times New Roman',
+#         legend_title_font_color="grey"
+#     )   
+#     return line_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
