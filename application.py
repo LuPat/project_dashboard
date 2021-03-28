029a7098a3ed529e9ccb287693a69f617c761292
Lutz Kinschert <68464349+LuPat@users.noreply.github.com>
2021-03-28 01:26:34 +0100

    add plotly graph & callbacks

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from functions import clean_datetime
 
-#pio.templates.default = 'plotly_dark'
+pio.templates.default = 'plotly'
 
 # Import Data, set 'date' column to datetime and as index
 cars = pd.read_csv('./data/car_builds_long_format.csv', index_col=0, parse_dates=True)
@@ -44,17 +44,26 @@ card_main = dbc.Card(
             [
                 # html.H4("Learn Dash with Charming Data", className="card-title"),
                 # html.H6("Please choose your options"),
-                html.P(
+                html.H6(
                     "Please choose region and customer",
                     className="card-text",
                 ),
-                dbc.Button("Europe", id="eu_button", color="primary"),
-                dbc.Button("Midlle East / Africa", id="mea_button", color="primary"),
+                dbc.FormGroup([
+                    
+                    dbc.Checklist(
+                        options=region, value=[], id="checklist_region", inline=True
+                    ),
+                ]),
+                #     'Europe', id='eu_button', value=region, color='primary'),
+                # dbc.Button('Middle East / Africa', id='mea', color='primary'),
+                # ], size='sm'),
+                # dbc.Button("Europe", id="eu_button", color="primary"),
+                # dbc.Button("Midlle East / Africa", id="mea_button", color="primary"),
                 dcc.Dropdown(id='customer_choice', options=customer,
                              value='BMW', placeholder='select Customer', clearable=True, style={"color": "#000000"}),
                 dcc.Dropdown(id='plant_choice', options=plant,
-                             value=2007, clearable=True, style={"color": "#000000"}),
-                # dbc.CardLink("GirlsWhoCode", href="https://girlswhocode.com/", target="_blank"),
+                              value=' ', clearable=True, style={"color": "#000000"}),
+                 #dbc.CardLink("GirlsWhoCode", href="https://girlswhocode.com/", target="_blank"),
             ]
         ),
     ],
@@ -128,15 +137,26 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
+    Output('customer_choice', 'options'),
+    Output('customer_choice', 'value'),
+    [Input('checklist_region', 'value')]
+)
+def update_dropdown(selected_region):
+    df = cars
+    filter_region = df[df['region'] == selected_region]
+    return country_options
+
+@app.callback(
     Output('carbuilds_graph', 'figure'),
-    Input('customer_choice', 'value')
+    [Input('customer_choice', 'value')],
+    [Input('customer_choice', 'value')]
     )
 
-def update_graph(selected_customer):
+def update_graph(selected_customer, selected_data):
     df = cars
-    df = df.groupby(['date']).sum().reset_index()
-    #customer_filter = df[df['customer'] == selected_customer]
-    line_fig = line_fig = px.bar(df, x='date', y='carbuilds', title='Time Series with Rangeslider')
+    df = df.groupby(['region', 'country', 'customer', 'date']).sum().reset_index()
+    customer_filter = df[df['customer'] == selected_customer]
+    line_fig = line_fig = px.bar(customer_filter, x='date', y='carbuilds', title=f'Carbuilds in {selected_customer}')
     line_fig.update_xaxes(rangeslider_visible=True)
     line_fig.update_xaxes(
         rangeslider_visible=True,
@@ -148,8 +168,13 @@ def update_graph(selected_customer):
                 dict(count=1, label="1y", step="year", stepmode="backward"),
                 dict(step="all")
         ])
-    )
-)   
+    ))
+    line_fig.update_layout(
+        font_family='Courier New',
+        font_color='Black',
+        title_font_family='Times New Roman',
+        legend_title_font_color="grey"
+    )   
     return line_fig
 
 if __name__ == '__main__':
